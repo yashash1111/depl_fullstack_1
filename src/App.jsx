0ee52592bb5175c0505bec458097be0d352d5ee1
yashash1111 <yashash.r.s70@kalvium.community>
2025-02-12 14:22:21 +0530

    commited

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>YASHASH R GOWDA</h1>
+      </div>
+       <div>
+        <h2>Phone number: 9623451111</h2>
+       </div>
+    </>
+  )
+}
+
+export default App
